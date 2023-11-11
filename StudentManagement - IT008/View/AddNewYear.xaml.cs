999b3cbf482615b761edaccf9f606d01d3bed826
Phạm Việt <74823960+HatakaCder@users.noreply.github.com>
2023-11-11 21:32:34 +0700

    Add AddNewYear file

---
+++ StudentManagement - IT008/View/AddNewYear.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManagement___IT008.View
+{
+    /// <summary>
+    /// Interaction logic for AddNewYear.xaml
+    /// </summary>
+    public partial class AddNewYear : UserControl, INotifyPropertyChanged
+    {
+        private DateTime today;
+        public DateTime Today
+        {
+            get { return today; }
+            set
+            {
+                if (today != value)
+                {
+                    today = value;
+                    OnPropertyChanged("Today");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+        public AddNewYear()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Today = DateTime.Today;
+        }
+    }
+}
