36257c19d603ed5ef9f9e448687ad60466f22549
Lường Đại Phát <21522443@gm.uit.edu.vn>
2023-11-24 11:40:55 +0700

    Last change

--- StudentManagement - IT008/View/Home1.xaml.cs
+++ StudentManagement - IT008/View/Home1.xaml.cs
@@ -24,5 +24,10 @@ namespace StudentManagement___IT008.View
         {
             InitializeComponent();
         }
+
+        private void Data_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
