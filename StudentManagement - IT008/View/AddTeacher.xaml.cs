8610309230f71685984ff1610fb40c615357f63c
Trung Kien Nguyen <koke36354@gmail.com>
2023-12-31 02:33:00 +0700

    Setup for Teacher UI done

--- StudentManagement - IT008/View/AddTeacher.xaml.cs
+++ StudentManagement - IT008/View/AddTeacher.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentManagement___IT008.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,13 +20,120 @@ namespace StudentManagement___IT008.View
     /// </summary>
     public partial class AddTeacher : Window
     {
+        List<string> gioitinhList = new List<string>
+        {
+            "Nam",
+            "Nữ"
+        };
         public AddTeacher()
         {
             InitializeComponent();
+            LabelToShow.Text = "Thêm giáo viên";
+            foreach (string gt in gioitinhList)
+            {
+                Gioitinh.Items.Add(new ComboBoxItem { Content = gt });
+            }
+            foreach (MONHOC mh in Entity.ins.MONHOCs)
+            {
+                Monday.Items.Add(mh.TENMH);
+            }    
+            Gioitinh.SelectedIndex = 0;
+            Monday.SelectedIndex = 0;
+        }
+        public AddTeacher(TAIKHOAN fixTK)
+        {
+            InitializeComponent();
+            LabelToShow.Text = "Chỉnh sửa giáo viên";
+            Username.IsEnabled = false;
+            foreach (string gt in gioitinhList)
+            {
+                Gioitinh.Items.Add(new ComboBoxItem { Content = gt });
+            }
+            foreach (MONHOC mh in Entity.ins.MONHOCs)
+            {
+                Monday.Items.Add(mh.TENMH);
+            }
+            Gioitinh.SelectedIndex = 0;
+            Monday.SelectedIndex = 0;
+            Hoten.Text = fixTK.HOTEN;
+            Ngsinh.SelectedDate = DateTime.Parse(fixTK.NGSINHSHOW);
+            Email.Text = fixTK.EMAIL;
+            Dchi.Text = fixTK.DCHI;
+            Username.Text = fixTK.USERNAME;
+            Password.Text = fixTK.PASSWRD;
+            Hocvi.Text = fixTK.HOCVI;
+            if (fixTK.GIOITINHSHOW == "Nam")
+                Gioitinh.SelectedIndex = 0;
+            else
+            {
+                Gioitinh.SelectedIndex = 1;
+            }
         }
         private void CloseClick(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            if (LabelToShow.Text == "Thêm giáo viên")
+            {
+                TAIKHOAN gv = new TAIKHOAN
+                {
+                    HOTEN = Hoten.Text,
+                    NGSINH = DateTime.Parse(Ngsinh.Text),
+                    EMAIL = Email.Text,
+                    DCHI = Dchi.Text,
+                    ISDELETED = false,
+                    GIOITINH = ((Gioitinh.Text == "Nam") ? true : false),
+                    USERNAME = Username.Text,
+                    PASSWRD = Password.Text,
+                    VAITRO = "GV"
+                };
+                GIAOVIEN newgv = new GIAOVIEN
+                {
+                    MAGV = Magv.Text,
+                    USERNAME = Username.Text,
+                    HOCVI = Hocvi.Text
+                };
+                KHANANGGIANGDAY ab = Entity.ins.KHANANGGIANGDAYs.SingleOrDefault(c => c.MAMH == Monday.Text);
+                newgv.KHANANGGIANGDAYs.Add(ab);
+                gv.GIAOVIENs.Add(newgv);
+                Entity.ins.TAIKHOANs.Add(gv);
+                try
+                {
+                    Entity.ins.SaveChanges();
+                    MessageBox.Show("Đã thêm học sinh thành công!", "Thông báo");
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            MessageBox.Show($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    Entity.ins.SaveChanges();
+                    MessageBox.Show("Đã chỉnh sửa học sinh thành công!", "Thông báo");
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            MessageBox.Show($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- StudentManagement - IT008/View/TeachersList.xaml.cs
+++ StudentManagement - IT008/View/TeachersList.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StudentManagement___IT008.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +22,91 @@ namespace StudentManagement___IT008.View
     /// </summary>
     public partial class TeachersList : UserControl
     {
+        ObservableCollection<TAIKHOAN> taikhoanList = new ObservableCollection<TAIKHOAN>();
         public TeachersList()
         {
             InitializeComponent();
+            foreach (TAIKHOAN tk in Entity.ins.TAIKHOANs.ToList())
+            {
+                if (tk.ISDELETED == false && tk.VAITRO == "GV")
+                    taikhoanList.Add(tk);
+            }
+            Data.ItemsSource = taikhoanList;
         }
 
         private void AddTeacherButton(object sender, RoutedEventArgs e)
         {
             AddTeacher addTeacher = new AddTeacher();
             addTeacher.ShowDialog();
+        }
+        private void CheckTeacherClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CheckAllTeachersClick(object sender, RoutedEventArgs e) 
+        {
+            foreach (var item in Data.Items)
+            {
+                var row = Data.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+
+                if (row != null)
+                {
+                    CheckBox checkBox = FindVisualChild<CheckBox>(row);
+
+                    if (checkBox != null)
+                    {
+                        checkBox.IsChecked = true;
+                    }
+                }
+            }
+        }
+        private void UncheckAllTeachersClick(object sender, RoutedEventArgs e) 
+        {
+            foreach (var item in Data.Items)
+            {
+                var row = Data.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+
+                if (row != null)
+                {
+                    CheckBox checkBox = FindVisualChild<CheckBox>(row);
+
+                    if (checkBox != null)
+                    {
+                        checkBox.IsChecked = false;
+                    }
+                }
+            }
+        }
+        private static T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child != null && child is T)
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+
+                    if (childOfChild != null)
+                    {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+        private void ChangeTTGiaoVien(object sender, RoutedEventArgs e) 
+        {
+            var button = (Button)sender;
+            TAIKHOAN dataItem = (TAIKHOAN)button.DataContext;
+            AddTeacher changeTTStudent = new AddTeacher(dataItem);
+            changeTTStudent.ShowDialog();
+            Data.ItemsSource = null;
+            Data.ItemsSource = taikhoanList;
         }
     }
 }
