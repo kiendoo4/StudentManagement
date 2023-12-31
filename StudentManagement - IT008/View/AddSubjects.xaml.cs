655746da69380591f00c87d3c03dbbe94e1d94bb
Trung Kien Nguyen <koke36354@gmail.com>
2024-01-01 06:28:50 +0700

    finish Monhoc part in Setting

--- StudentManagement - IT008/View/AddSubjects.xaml.cs
+++ StudentManagement - IT008/View/AddSubjects.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StudentManagement___IT008.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,95 @@ namespace StudentManagement___IT008.View
     /// </summary>
     public partial class AddSubjects : UserControl
     {
-        public AddSubjects()
+        bool addorchange = true;
+        ObservableCollection<MONHOC> monhocList2 = new ObservableCollection<MONHOC>();
+        MONHOC monhoc2 = new MONHOC();
+        public AddSubjects(ObservableCollection<MONHOC> monhocList)
         {
             InitializeComponent();
+            monhocList2 = monhocList;
+            Mamh.IsEnabled = false;
+            int cnt = 1;
+            foreach (MONHOC countMH in Entity.ins.MONHOCs)
+            {
+                if (countMH.ISDELETED == false)
+                    cnt++;
+            }
+            Mamh.Text = "MH" + Convert.ToString(cnt);
+        }
+        public AddSubjects(ObservableCollection<MONHOC> monhocList, MONHOC mh)
+        {
+            InitializeComponent();
+            monhocList2 = monhocList;
+            monhoc2 = mh;
+            Mamh.IsEnabled = false;
+            addorchange = false;
+            Mamh.Text = mh.MAMH;
+            Tenmh.Text = mh.TENMH;
+        }
+
+        private void FinishClick(object sender, RoutedEventArgs e)
+        {
+            if(addorchange)
+            {
+                MONHOC newMh = new MONHOC();
+                int cnt = 1;
+                foreach (MONHOC countMH in Entity.ins.MONHOCs)
+                {
+                    if (countMH.ISDELETED == false)
+                        cnt++;
+                }
+                newMh.MAMH = Mamh.Text;
+                newMh.TENMH = Tenmh.Text;
+                newMh.ISDELETED = false;
+                Entity.ins.MONHOCs.Add(newMh);
+                try
+                {
+                    Entity.ins.SaveChanges();
+                    MessageBox.Show("Đã thêm môn học thành công!", "Thông báo");
+                    monhocList2.Clear();
+                    foreach (MONHOC mh in Entity.ins.MONHOCs.ToList())
+                    {
+                        if (mh.ISDELETED == false)
+                            monhocList2.Add(mh);
+                    }
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            MessageBox.Show($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                        }
+                    }
+                }
+            }   
+            else
+            {
+                monhoc2.TENMH = Tenmh.Text;
+                try
+                {
+                    Entity.ins.SaveChanges();
+                    MessageBox.Show("Đã chỉnh sửa môn học thành công!", "Thông báo");
+                    monhocList2.Clear();
+                    foreach (MONHOC mh in Entity.ins.MONHOCs.ToList())
+                    {
+                        if (mh.ISDELETED == false)
+                            monhocList2.Add(mh);
+                    }
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    foreach (var validationErrors in ex.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            MessageBox.Show($"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}");
+                        }
+                    }
+                }
+            }    
         }
     }
 }
--- StudentManagement - IT008/View/Setting.xaml.cs
+++ StudentManagement - IT008/View/Setting.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StudentManagement___IT008.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +22,91 @@ namespace StudentManagement___IT008.View
     /// </summary>
     public partial class Setting : UserControl
     {
+        ObservableCollection<MONHOC> monhocList = new ObservableCollection<MONHOC>();
         public Setting()
         {
             InitializeComponent();
+            foreach (MONHOC mh in Entity.ins.MONHOCs.ToList())
+            {
+                if (mh.ISDELETED == false)
+                    monhocList.Add(mh);
+            }
+            Data.ItemsSource = monhocList;
         }
 
+        private void CheckAllMonhocsClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in Data.Items)
+            {
+                var row = Data.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
 
+                if (row != null)
+                {
+                    CheckBox checkBox = FindVisualChild<CheckBox>(row);
+
+                    if (checkBox != null)
+                    {
+                        checkBox.IsChecked = true;
+                    }
+                }
+            }
+        }
+        private void UncheckAllMonhocsClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in Data.Items)
+            {
+                var row = Data.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+
+                if (row != null)
+                {
+                    CheckBox checkBox = FindVisualChild<CheckBox>(row);
+
+                    if (checkBox != null)
+                    {
+                        checkBox.IsChecked = false;
+                    }
+                }
+            }
+        }
+        private static T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child != null && child is T)
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+
+                    if (childOfChild != null)
+                    {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
         private void AddSubjectButton(object sender, RoutedEventArgs e)
         {
             AddMonHocUC.Children.Clear();
-            AddSubjects addSubjects = new AddSubjects();
+            AddSubjects addSubjects = new AddSubjects(monhocList);
             AddMonHocUC.Children.Add(addSubjects);
-        }
+            AddorFixSubjects.Text = "Thêm môn học";
 
+        }
+        private void ChangeTTMonhoc(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            MONHOC dataItem = (MONHOC)button.DataContext;
+            AddMonHocUC.Children.Clear();
+            AddSubjects addSubjects = new AddSubjects(monhocList, dataItem);
+            AddMonHocUC.Children.Add(addSubjects);
+            AddorFixSubjects.Text = "Chỉnh sửa môn học";
+        }
         private void DeleteSubjectButton(object sender, RoutedEventArgs e)
         {
 
